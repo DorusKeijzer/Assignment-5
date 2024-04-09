107f400d770d1f2545b196afce2672ab5bdd553d
Dorus Keijzer <83274869+DorusKeijzer@users.noreply.github.com>
2024-04-09 11:34:19 +0200

    changed optical flow architecture

--- model_architectures/optical_flow/optical_flow_model.py
+++ model_architectures/optical_flow/optical_flow_model.py
@@ -15,38 +15,59 @@ class model(nn.Module):
         self.conv1 = nn.Conv2d(in_channels=2, out_channels=16, kernel_size=3, padding=1)
         self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1)
         self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
+        self.conv4 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1)
         
-        # Max pooling layer
-        self.pool = nn.AdaptiveMaxPool2d((1, 1))
+        # pooling layers
+        self.adaptivepool = nn.AdaptiveMaxPool2d((128, 128))
+        self.maxpool = nn.MaxPool2d((2,2),2)
+
         # Fully connected layers
-        self.fc1 = nn.Linear(64, 128)
-        self.fc2 = nn.Linear(128, 12)
+        self.fc1 = nn.Linear(4096, 256)
+        self.fc2 = nn.Linear(256, 64)
+        self.fc3 = nn.Linear(64, 12)
         
         # dropout layer
         self.dropout = nn.Dropout(0.5)
+        
+        self.init_weights()
+
+    def init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.xavier_normal_(m.weight)
 
     def forward(self, x):
         # Input size: [batch_size, channels, height, width]
         # Transpose the input tensor to [batch_size, channels, height, width]
         x = x.permute(0, 3, 1, 2)
+        # x = x.permute(1, 0, 3, 2)
         # Convolutional layers
         x = F.relu(self.conv1(x))
-        x = self.pool(x)
+        x = self.adaptivepool(x)
         x = F.relu(self.conv2(x))
+        x = self.maxpool(x)
         x = F.relu(self.conv3(x))
+        x = self.maxpool(x)
+        x = F.relu(self.conv4(x))
+        x = self.maxpool(x)
+        x = F.relu(self.conv4(x))
+        x = self.maxpool(x)
 
-        
         # Flatten the output for fully connected layers
         x = torch.flatten(x, 1)
         
         # Fully connected layers
         x = F.relu(self.fc1(x))
         x = self.dropout(x)
-        x = F.softmax(self.fc2(x), dim=1)
+        x = F.relu(self.fc2(x))
+        x = F.softmax(self.fc3(x), dim=1)
         
         return x
     
 
     
 if __name__ == "__main__":
-    pass
+    from torchsummary import summary
+    model = model()
+    print(model.name)
+    summary(model, (1, 260, 400))
